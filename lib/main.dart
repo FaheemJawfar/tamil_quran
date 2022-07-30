a70dd05fe89ac6daf349618b0ed1e690a308940b
FaheemJawfar <faheemjawfar@gmail.com>
2022-07-30 06:13:25 +0530

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './ReadQuran.dart';
+import './sura_namelist.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.green,
       ),
       debugShowCheckedModeBanner: false,
-      home: ReadQuran(),
+      home: SuraNames(),
     );
   }
 }
