748838b2148e19463b414dfde6beebdb434ec71b
Faheem <faheemjawfar@gmail..com>
2024-12-26 14:56:20 +0300

    Last read arabic page function added.

--- lib/home/sura_list_arabic_screen.dart
+++ lib/home/sura_list_arabic_screen.dart
@@ -5,6 +5,7 @@ import '../app_texts/home_texts.dart';
 import '../read_quran/sura_details.dart';
 import '../read_quran/sura_arabic_screen.dart';
 import '../providers/quran_provider.dart';
+import '../utils/shared_preferences.dart';
 
 class SuraListArabicScreen extends StatefulWidget {
   const SuraListArabicScreen({Key? key}) : super(key: key);
@@ -23,7 +24,33 @@ class _SuraListArabicScreenState extends State<SuraListArabicScreen> {
       backgroundColor:
           quranProvider.isDarkMode ? null : ColorConfig.backgroundColor,
       body: Column(
+
         children: [
+                  ElevatedButton.icon(
+  onPressed: () {
+    
+  int pageNumber = AppPreferences.getInt('lastPageNumber') ?? 1;
+  int suraNumber = AppPreferences.getInt('lastSuraNumber') ?? 1;
+  quranProvider.selectedSuraNumber = suraNumber;
+
+  print("Navigating to Sura Number: $suraNumber");
+  print("Navigating to Page Number: $pageNumber");
+
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => SuraArabicScreen(initialPageNumber: pageNumber),
+    ),
+  );
+
+  },
+  label: const Text('Continue...'),
+  icon: const Icon(Icons.menu_book_sharp),
+  style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.green, // Replace with your desired color
+  ),
+),
+
           Expanded(
             child: ListView.separated(
               itemCount: SuraDetails.suraListAll.length,
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:provider/provider.dart';
 import 'package:tamil_quran/app_config/update_config.dart';
-import 'package:tamil_quran/quran_test.dart';
 import 'app_config/app_config.dart';
 import 'utils/shared_preferences.dart';
 import '../providers/quran_provider.dart';
